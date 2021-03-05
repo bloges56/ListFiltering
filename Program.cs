f48f7e70432a8b5517eb5d8eaaec03d7bea2cd6b
bloges56 <brady.drew.logan@gmail.com>
2021-03-05 15:55:05 -0600

    filters out the integers from a list

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ListFiltering
 {
@@ -7,6 +9,21 @@ namespace ListFiltering
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            IEnumerable<object> nums = listOfItems.Where(item =>
+            {
+                return item is int;
+            });
+
+            IEnumerable<int> ints = nums.Select(num =>
+            {
+                return (int)num;
+            });
+
+            return ints;
         }
     }
 }
